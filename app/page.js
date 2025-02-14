4ede378e20933e336e808324cd3b1d5c692d407e
errorI404I <ignacio.dellac@gmail.com>
2025-02-14 10:49:43 -0800

    Corrección de errores en build

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link'; // Cambia a la importación de Next.js
 
 const Home = () => {
     return (
@@ -6,6 +7,11 @@ const Home = () => {
             <h1 className="text-4xl font-bold text-blue-600">
                 Bienvenido al Centro de Entrenamiento
             </h1>
+            <div className="mt-4">
+                <Link href="/login" className="text-blue-500 hover:underline">Iniciar Sesión</Link>
+                <Link href="/image-upload" className="ml-4 text-blue-500 hover:underline">Subir Imagen</Link>
+                <Link href="/about" className="ml-4 text-blue-500 hover:underline">Ir a "Quiénes Somos"</Link>
+            </div>
         </div>
     );
 };
